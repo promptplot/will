28dc4e109482336159b80d70293c513f508f7630
zeru15 <zeruf5@gmail.com>
2022-10-11 23:25:00 +0300

    added file upload

--- src/pages/TheWill.tsx
+++ src/pages/TheWill.tsx
@@ -190,6 +190,18 @@ const TheWill = ({ page, setPage, formData, setFormData }) => {
                 </div>
             </div>
 
+
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 font-bold " htmlFor="file_input">
+                Upload file
+            </label>
+            <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 
+            cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 
+            dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" />
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">
+                SVG, PNG, JPG or GIF (MAX. 800x400px).
+            </p>
+
+
             <button
                 onClick={() => {
                     setPage(page + 1);
